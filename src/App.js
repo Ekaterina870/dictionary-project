0df92750b303b65c0efd64af1a1f3dbfb3f843bf
Ekaterina870 <122622907+Ekaterina870@users.noreply.github.com>
2023-05-19 14:29:39 +0100

    search engine added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Dictionary App</h1>
-      <button className="btn btn-primary">Hello</button>
+      <div className="container">
+        <header>
+          <h1>Dictionary App</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a
+              href="https://github.com/Ekaterina870/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-source code
+            </a>{" "}
+            by Ekaterina Volkova
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
